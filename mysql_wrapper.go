0b1bed8fd6bf8fc222d2dd16cd30af79719e0b6e
Viresh <asviresh@gmail.com>
2014-09-15 19:07:11 +0530

    changes to mysql wrapper

--- mysql_wrapper.go
+++ mysql_wrapper.go
@@ -11,7 +11,7 @@ func GetMysqlClient(hostPort string) *sql.DB {
 }
 
 func createMysqlPool(hostNPorts []string) interface{} {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/bm")
+	db, err := sql.Open("mysql", hostNPorts[0])
 	if err != nil {
 		return nil
 	}
