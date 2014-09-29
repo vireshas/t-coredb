aea0aa9bd81284c8dcffbbbe78680523d8713225
viresh a s <asviresh@gmail.com>
2014-09-29 19:37:40 +0530

    mutex arnd map; handling concurrent access to connection map

--- pool_manager.go
+++ pool_manager.go
@@ -1,12 +1,20 @@
 package db
 
+import (
+	"sync"
+)
+
+//let's protect concurrent access to the map
+var rwMutex sync.RWMutex
 var IpToPoolMap = make(map[string]interface{})
 
 type createPoolCallBack func(hostNPort []string) interface{}
-
 type PoolManager struct{}
 
 func (m PoolManager) GetConnection(cb createPoolCallBack, hostNPort string) interface{} {
+	rwMutex.Lock()
+	defer rwMutex.Unlock()
+
 	pool, ok := IpToPoolMap[hostNPort]
 	if !ok {
 		hostAndPort := []string{hostNPort}
